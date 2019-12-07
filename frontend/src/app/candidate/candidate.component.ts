ff5049c8094de096a474fa577c6322c8df4e9c73
TiffanyVPhan <tiffany.phan@colorado.edu>
2019-12-07 11:23:46 -0700

    added components

---
+++ frontend/src/app/candidate/candidate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate',
+  templateUrl: './candidate.component.html',
+  styleUrls: ['./candidate.component.css']
+})
+export class CandidateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
